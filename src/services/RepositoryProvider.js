59498213c279da7a015c758e72960a6ab00984c2
cm <christian.menzel@connedata.de>
2018-05-14 17:07:10 +0200

    bugfix resource.data

--- src/services/RepositoryProvider.js
+++ src/services/RepositoryProvider.js
@@ -36,10 +36,10 @@
       function get(options) {
         return _findResource(_path, options).then(function(resource) {
           var data = _parse(resource);
-          if (resource.data.meta) {
+          if (resource.meta) {
             data = angular.extend(data, {
               meta: function() {
-               return resource.data.meta;
+               return resource.meta;
               }
             });
           }
